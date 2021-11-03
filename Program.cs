7cca575c381a33b4d4d93adfcce45c55c9c78917
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 19:59:59 -0300

    Adiciona atividade Negativo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            Single.TryParse(Console
+            .ReadLine()
+            .ToString(), out Single _numeroDigitado);
+
+            if(_numeroDigitado < 0){
+                Console.WriteLine("Você digitou um número negativo.");
+            }
+        }
+    }
+}
